75daaedeecdedea70eccd5f3f94b2b9d7964b2da
howetuft <howetuft@gmail.com>
2025-11-08 08:43:12 +0100

    Remove intermediary Conan files

---
+++ utils/clean-conan-files.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Cleaning Conan intermediary files"
+
+
+files=(
+  "conanbuildenv-release-x86_64.sh" \
+  "conanbuild.sh" \
+  "conanrunenv-release-x86_64.sh" \
+  "conanrun.sh" \
+  "deactivate_conanbuildenv-release-x86_64.sh" \
+  "deactivate_conanbuild.sh" \
+  "deactivate_conanrun.sh" \
+  "graph.json" \
+  "list.json" \
+)
+
+for file in "${files[@]}"; do
+  echo "- Removing ${file}"
+  rm -f "$file"
+done
+
+
+echo "- Removing conan-center-index (folder)"
+rm -rf conan-center-index
